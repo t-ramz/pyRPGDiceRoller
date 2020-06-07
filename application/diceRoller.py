f5a4b410934d26644154f2ea4779dd2affa83e76
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2020-06-06 21:25:32 -0500

    separate code into sections related to dice

--- application/diceRoller.py
+++ application/diceRoller.py
@@ -1,8 +1,27 @@
 import random
 import time
 
+# D8 code
 min = 1
 max = 8
+
+print(random.randint(min, max))
+
+
+random.seed(time.time())
+
+# DVirgil code
+min = 1
+max = 50
+
+print(random.randint(min, max))
+
+
+random.seed(time.time())
+
+# D12 code
+min = 1
+max = 12
 
 print(random.randint(min, max))
 
