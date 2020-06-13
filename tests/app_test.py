cf09c76531311d196c0adcfaa7a07fe2d4671f47
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2020-06-12 22:08:03 -0500

    Add code for each die test

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,10 +1,53 @@
 import sys
 sys.path.append('../')
 
-from pyRPGDiceRoller import diceRoller
+from pyRPGDiceRoller.application import diceRoller
+
+
+def test_d100(capsys):
+    diceRoller.d100()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 100))
+    assert capture.out in myTestList
+
+
+def test_d20(capsys):
+    diceRoller.d20()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 20))
+    assert capture.out in myTestList
+
+
+def test_d12(capsys):
+    diceRoller.d12()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 12))
+    assert capture.out in myTestList
 
 
 def test_d10(capsys):
     diceRoller.d10()
     capture = capsys.readouterr()
-    assert capture.out == "8\n"
+    myTestList = (str(s) + "\n" for s in range(1, 10))
+    assert capture.out in myTestList
+
+
+def test_d8(capsys):
+    diceRoller.d8()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 8))
+    assert capture.out in myTestList
+
+
+def test_d6(capsys):
+    diceRoller.d6()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 6))
+    assert capture.out in myTestList
+
+
+def test_d4(capsys):
+    diceRoller.d4()
+    capture = capsys.readouterr()
+    myTestList = (str(s) + "\n" for s in range(1, 4))
+    assert capture.out in myTestList
