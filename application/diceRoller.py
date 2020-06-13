cbf31e9bdde3024912706873c85a627fb0585739
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2020-06-12 22:20:27 -0500

    Remove unnecessary seeding, random library handles it

--- application/diceRoller.py
+++ application/diceRoller.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 # D8 code
 min = 1
@@ -8,25 +7,17 @@ max = 8
 print(random.randint(min, max))
 
 
-random.seed(time.time())
-
 # DVirgil code
 min = 1
 max = 50
 
 print(random.randint(min, max))
 
-
-random.seed(time.time())
-
 # D12 code
 min = 1
 max = 12
 
 print(random.randint(min, max))
-
-
-random.seed(time.time())
 
 
 # D10 code
