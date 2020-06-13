9a1402dcabc90ca80751e3becf963217c5ab2e9f
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2020-06-12 22:23:13 -0500

    Convert existing code into functions

--- application/diceRoller.py
+++ application/diceRoller.py
@@ -1,23 +1,28 @@
 import random
 
-# D8 code
-min = 1
-max = 8
 
-print(random.randint(min, max))
+# D8 code
+def d8():
+    min = 1
+    max = 8
+
+    print(random.randint(min, max))
 
 
 # DVirgil code
-min = 1
-max = 50
+def DVirgil():
+    min = 1
+    max = 50
 
-print(random.randint(min, max))
+    print(random.randint(min, max))
+
 
 # D12 code
-min = 1
-max = 12
+def d12():
+    min = 1
+    max = 12
 
-print(random.randint(min, max))
+    print(random.randint(min, max))
 
 
 # D10 code
@@ -26,4 +31,7 @@ def d10():
     print(random.randint(1, 10))
 
 
+d8()
 d10()
+d12()
+DVirgil()
