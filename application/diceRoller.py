10cd2a7e2cb80ec9be5620ee01d602c9b5a33bfc
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2020-06-22 20:29:16 -0500

    Add remaining dice closes #9 #8 #7 #3

--- application/diceRoller.py
+++ application/diceRoller.py
@@ -1,33 +1,53 @@
 import random
 
 
-# D8 code
 def d8():
+    '''this will print the roll of a d8'''
     min = 1
     max = 8
 
     print(random.randint(min, max))
 
 
-# DVirgil code
 def DVirgil():
+    '''this will print the roll of a d50'''
     min = 1
     max = 50
 
     print(random.randint(min, max))
 
 
-# D12 code
 def d12():
+    '''this will print the roll of a d12'''
     min = 1
     max = 12
 
     print(random.randint(min, max))
 
 
-# D10 code
 def d10():
+    '''this will print the roll of a d10'''
     print(random.randint(1, 10))
+
+
+def d4():
+    '''this will print the roll of a d4'''
+    print(random.randint(1, 4))
+
+
+def d6():
+    '''this will print the roll of a d6'''
+    print(random.randint(1, 6))
+
+
+def d20():
+    '''this will print the roll of a d20'''
+    print(random.randint(1, 20))
+
+
+def d100():
+    '''this will print the roll of a d100'''
+    print(random.randint(1, 100))
 
 
 d8()
